8048009c611b5c0a277f88f2c81994dca2270a3f
Mili Antoni <miliantoni12@gmail.com>
2024-07-02 23:36:02 -0300

    ejercicio 6

---
+++ ejercicio-6/js/ejercicio6.js
@@ -0,0 +1,10 @@
+let numero1 = parseFloat (prompt("Ingresar numeros"));
+let numero2 = parseFloat(prompt("Ingresar numeros"));
+
+if (numero1 > numero2 ) {
+    document.write("el numero mayor es", numero1);
+} 
+    else{
+        document.write("el numero mayor es", numero2);
+    }
+
